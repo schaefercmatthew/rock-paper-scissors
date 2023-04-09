4a267d28246c9970d9c2978aaf70f577e877cacc
schaefercmatthew <schaefercmatthew@gmail.com>
2023-04-09 13:55:09 -0400

    Finish first full version

--- r-p-s.js
+++ r-p-s.js
@@ -4,27 +4,29 @@ let results = [];
 function RPS(computerSelection, playerSelection) {
   switch (computerSelection.concat(playerSelection)) {
     case "RP":
-      results.push("Paper covers Rock: You win!");
+      results.push("Paper covers Rock: You won a round?!");
       break;
     case "SR":
-      results.push("Rock crushes Scissors: You win!");
+      results.push("Rock crushes Scissors: Wow, you're doing it!");
       break;
     case "PS":
-      results.push("Scissors cuts Paper: You win!");
+      results.push("Scissors cuts Paper: Okay, now keep winning!");
       break;
     case "RR":
     case "PP":
     case "SS":
-      results.push("So did the computer: A cats game!");
+      results.push(
+        "So did the computer: Come on. Do better. The whole world is counting on you."
+      );
       break;
     case "PR":
-      results.push("Paper covers Rock: You lose!");
+      results.push(":( Paper covers Rock: We're dooooooomed.");
       break;
     case "RS":
-      results.push("Rock crushes Scissors: You lose!");
+      results.push(":( Rock crushes Scissors: Ahh, stop doing that.");
       break;
     case "SP":
-      results.push("Scissors cuts Paper: You lose!");
+      results.push(":( Scissors cuts Paper: Geez. Are you even trying?");
       break;
   }
 }
@@ -45,6 +47,25 @@ const rockButton = document.querySelector("#rockButton");
 const scissorsButton = document.querySelector("#scissorsButton");
 const paperButton = document.querySelector("#paperButton");
 
+// scoring
+let playerScore = 0;
+let computerScore = 0;
+
+const scoreDisplay = document.getElementById("score");
+
+function updateScore() {
+  scoreDisplay.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
+  if (playerScore === 5) {
+    window.alert("Player wins! You *actually* saved us!", "Keep Playing?");
+    location.reload();
+    window.scrollTo(0, 0);
+  } else if (computerScore === 5) {
+    window.alert("Computer wins. Thanks a lot.", "Try again? Please.");
+    location.reload();
+    window.scrollTo(0, 0);
+  }
+}
+
 rockButton.addEventListener("click", () => {
   let displayMessage = document.getElementById("dispMessage");
   displayMessage.textContent = ""; // clear old message
@@ -54,6 +75,14 @@ rockButton.addEventListener("click", () => {
     RPS(computerSelection, playerSelection);
     let result = "You picked Rock! " + results[results.length - 1];
     displayMessage.textContent = result;
+
+    if (results[results.length - 1].includes("!")) {
+      playerScore++;
+      updateScore();
+    } else if (results[results.length - 1].includes(":(")) {
+      computerScore++;
+      updateScore();
+    }
   }, 1000); // wait for 1 second before displaying new message
 });
 
@@ -66,6 +95,14 @@ scissorsButton.addEventListener("click", () => {
     RPS(computerSelection, playerSelection);
     let result = "You picked Scissors! " + results[results.length - 1];
     document.getElementById("dispMessage").textContent = result;
+
+    if (results[results.length - 1].includes("!")) {
+      playerScore++;
+      updateScore();
+    } else if (results[results.length - 1].includes(":(")) {
+      computerScore++;
+      updateScore();
+    }
   }, 1000); // wait for 1 second before displaying new message
 });
 
@@ -78,6 +115,14 @@ paperButton.addEventListener("click", () => {
     RPS(computerSelection, playerSelection);
     let result = "You picked Paper! " + results[results.length - 1];
     document.getElementById("dispMessage").textContent = result;
+
+    if (results[results.length - 1].includes("!")) {
+      playerScore++;
+      updateScore();
+    } else if (results[results.length - 1].includes(":(")) {
+      computerScore++;
+      updateScore();
+    }
   }, 1000); // wait for 1 second before displaying new message
 });
 
@@ -85,8 +130,15 @@ paperButton.addEventListener("click", () => {
 const section1 = document.querySelector("#section1");
 const section2 = document.querySelector("#section2");
 const section3 = document.querySelector("#section3");
+const section4 = document.querySelector("#section4");
+const section5 = document.querySelector("#section5");
 
 // set the initial background images
-section1.style.backgroundImage = 'url("/Pictures/Mountain.jpg")';
-section2.style.backgroundImage = 'url("/Pictures/mountain2.jpg")';
-section3.style.backgroundImage = 'url("/Pictures/mountain3.jpg")';
+section1.style.backgroundImage = 'url("/citypics/city1.png")';
+section2.style.backgroundImage = 'url("/citypics/city2.png")';
+section3.style.backgroundImage = 'url("/citypics/city3.png")';
+section4.style.backgroundImage = 'url("/citypics/city4.png")';
+section5.style.backgroundImage = 'url("/citypics/city5.jpg")';
+
+// show the initial score
+updateScore();
